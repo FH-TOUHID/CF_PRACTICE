942e90430c7fc109840d5df7fa9e5f7e59f246a1
FH-TOUHID <touhid192005@gmail.com>
2025-09-20 03:42:11 +0600

    Update practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -775,19 +775,45 @@
 //     }
 // }
 
-#include <bits/stdc++.h>
-using namespace std;
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        long long n,k,x;cin>>n>>k>>x;
-        long long min=(k*(k+1))/2;
-        long long max=(n*(n+1))/2-((n-k)*(n-k+1))/2;
-        if(x>=min && x<=max) cout<<"YES"<<endl;
-        else cout<<"NO"<<endl;
-    }
-}
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main() {
+//     ios::sync_with_stdio(false);
+//     cin.tie(nullptr);
+//     int t;
+//     cin >> t;
+//     while (t--)
+//     {
+//         long long n,k,x;cin>>n>>k>>x;
+//         long long min=(k*(k+1))/2;
+//         long long max=(n*(n+1))/2-((n-k)*(n-k+1))/2;
+//         if(x>=min && x<=max) cout<<"YES"<<endl;
+//         else cout<<"NO"<<endl;
+//     }
+// }
+
+// B. Multiply by 2, divide by 6
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int t;cin>>t;
+//     while(t--)
+//     {
+//        long long n; cin>>n;
+//        long long c3=0;
+//        long long c2=0;
+//        while(n>0 && n%3==0)
+//        {
+//             c3++;
+//             n/=3;
+//        }
+//         while(n>0 && n%2==0)
+//        {
+//             c2++;
+//             n/=2;
+//        }
+//        if(n>1 || c3<c2) cout<<"-1"<<endl;
+//        else cout<<(c3-c2)+c3<<endl;
+//     }
+// }
